625fd082f4d11c607e88bec37945f71861e7e5b3
P-M-Manmohan <manu6520042@gmail.com>
2025-08-04 19:13:58 +0900

    finished day2 part1

--- 2024/day2/part1/src/main.rs
+++ 2024/day2/part1/src/main.rs
@@ -1,29 +1,58 @@
 use std::fs;
 
-fn main() {
-    let contents = fs::read_to_string("../test").expect("error reading file");
-    let s1: String = String::new();
-    let s2: String = String::new();
-    let order:i32 = -1;
-    for char in contents.char() {
-        if char == ' ' &&  s1.len()!=0{
-            s1 = s2;
-            s2.clear();
-            continue;
-        }else if char == '\n'{
-            s1.clear();
-            s2.clear();
-            order = -1;
-        }else if char != ' ' {
-            s2.push(char);
-        }else{
-            if order != -1{
-                if s1<s2 {
-                    order = 0;
-                } else if s1>s2 {
-                    order = 1;
-                }
-            }
+fn line_arr(arr: String) -> Vec<Vec<u32>> {
+    let mut list:Vec<Vec<u32>> = Vec::new();
+    let mut inner:Vec<u32> = Vec::new();
+    let mut s:String = String::new();
+    for char in arr.chars(){
+        if char != ' ' && char != '\n'{
+            s.push(char);
+        }else if char == ' '{
+           inner.push(s.parse().unwrap());
+           s.clear();
+        }else if char == '\n' {
+            inner.push(s.parse().unwrap());
+            s.clear();
+            list.push(inner.clone());
+            inner.clear();
         }
     }
+    return list;
+}
+
+fn main() {
+    let contents = fs::read_to_string("../input").expect("error reading file");
+    let arr = line_arr(contents);
+    let mut rep = 0;
+    for list in arr{
+        let mut safe = 1;
+        let mut order = -1;
+        let mut i = 1;
+        while i<list.len(){
+            let diff = list[i] as i32 - list[i-1] as i32;
+            if order ==-1{
+                if diff < 0 {
+                    order = 2;
+                }else if diff > 0{
+                    order = 1;
+                }else{
+                    safe = 0;
+                    break;
+                }
+            }
+            if order == 1 && ( diff <= 0 || diff >3 ){
+                    safe = 0;
+                   break;
+            }
+            if order == 2 && ( diff >= 0 || diff < -3 ){
+                    safe = 0;
+                   break;
+            }
+            i+=1;
+        }
+        if safe == 1 {
+            rep+=1;
+        }
+    }
+    println!("{rep:?}");
 }
